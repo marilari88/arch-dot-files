bca8eabb1e4b2998004e8a4151b269fcd9b799cc
Marco Ilari <marilari88@gmail.com>
2021-04-04 01:39:34 +0200

    Aggiunto conky

---
+++ .config/conky/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd $(dirname $0)
+killall conky 2>/dev/null
+echo "Conky waiting 5 seconds to start..."
+if conky --daemonize --quiet --config=./conky.conf ; then
+  echo "Started"
+else
+  echo "Failed"
+fi
